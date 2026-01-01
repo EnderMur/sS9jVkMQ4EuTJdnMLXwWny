7cf473f7e4660ca136c16578aa3187aa527b8030
EnderMur <126102663+EnderMur@users.noreply.github.com>
2026-01-01 05:42:13 +0300

    Перенесён мод Joining Screen Selector

--- Удобство/Joining Screen Selector/loc/localizations.lua
+++
@@ -1,7 +0,0 @@
-Hooks:Add("LocalizationManagerPostInit", "MenuManagerInitialize_JoiningScreenSelector", function(loc)
-	jss_languageList = {
-		"loc/english.txt"
-	}
-	jss_languagePath = jss_languageList[1]
-	loc:load_localization_file(JoiningScreenSelector.path .. jss_languagePath)
-end)
